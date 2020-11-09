388fbcfabaf205d3da5428dbbca168d6f64dc6c2
Tran Tung <trantung1197@gmail.com>
2020-11-09 17:34:02 +0700

    a

---
+++ module/mnmd-module-test-1.php
@@ -0,0 +1,85 @@
+<div class="mnmd-block mnmd-block--fullwidth mnmd-featured-module-test-1">
+    <div class="container">
+        <div class="block-heading block-heading--fullwidth block-heading--seperated-right">
+            <h3 class="block-heading__title">
+                Essential
+            </h3>
+        </div>
+
+        <div class="mnmd-block__inner mnmd-block__inner--fullwidth-right flex-box">
+            <div class="middle-section">
+                <div class="post">
+                    <div class="post__thumb post__thumb--height-600">
+                        <a href="#">
+                            <img src="http://via.placeholder.com/900x300" alt="img">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="left-section">
+                <div class="entry-interaction entry-interaction--vertical entry-interaction__text-rotate">
+                    <div class="post-sharing post-sharing__list-btn">
+                        <ul>
+                            <li>
+                                <a href="#" class="sharing-btn sharing-btn__square facebook-btn btn-active" data-toggle="tooltip" data-placement="top" title="" data-original-title="Share on Facebook"><i class="mdicon mdicon-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="sharing-btn sharing-btn__square twitter-btn" data-toggle="tooltip" data-placement="top" title="" data-original-title="Share on Twitter"><i class="mdicon mdicon-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a href="#" class="sharing-btn sharing-btn__square googleplus-btn" data-toggle="tooltip" data-placement="top" title="" data-original-title="Share on Google+"><i class="mdicon mdicon-google-plus"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="post-sharing post-sharing__text">
+                        <span class="sharing__text sharing__text-line-after">sharing</span>
+                    </div>
+                </div>
+            </div>
+            <div class="right-section">
+                <div class="posts-list flex-box-1i flex-box list-seperated">
+                    <div class="list-item">
+                        <article class="post post--vertical post--no-thumb post--background-active">
+                            <div class="post__text post__text--spacing-1 inverse-text">
+                                <div class="post__meta post__meta-heading post__meta-heading-ineverse">
+                                    <a href="#" class="post__cat">INSPIRATION</a>
+                                    <time class="time" datetime="none" title="none">August 20, 2019</time>
+                                </div>
+                                <h3 class="post__title f-24 f-w-500">
+                                    <a href="#">Stay Patient and Trust Your Own Journey</a>
+                                </h3>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="list-item">
+                        <article class="post post--vertical post--no-thumb">
+                            <div class="post__text post__text--spacing-1">
+                                <div class="post__meta post__meta-heading">
+                                    <a href="#" class="post__cat">LIFESTYLE</a>
+                                    <time class="time" datetime="none" title="none">August 20, 2019</time>
+                                </div>
+                                <h3 class="post__title f-24 f-w-500">
+                                    <a href="#">I Have Measured Out My Life With Coffee Spoons</a>
+                                </h3>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="list-item">
+                        <article class="post post--vertical post--no-thumb">
+                            <div class="post__text post__text--spacing-1">
+                                <div class="post__meta post__meta-heading">
+                                    <a href="#" class="post__cat">INSPIRATION</a>
+                                    <time class="time" datetime="none" title="none">August 20, 2019</time>
+                                </div>
+                                <h3 class="post__title f-24 f-w-500">
+                                    <a href="#">Stay Patient and Trust Your Own Journey</a>
+                                </h3>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ test-1.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <!-- Basic -->
+    <meta charset="UTF-8">
+
+    <title>The Next Mag - Ultimate News and Magazine Template</title>
+
+    <meta name="keywords" content="HTML5 News Magazine Template" />
+    <meta name="description" content="The Next Mag - Ultimate News and Magazine Template">
+    <meta name="author" content="minimaldog.net">
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/favicon.png" sizes="32x32" />
+    <link rel="icon" href="img/favicon.png" sizes="192x192" />
+    <link rel="apple-touch-icon-precomposed" href="img/favicon.png" />
+    <meta name="msapplication-TileImage" content="img/favicon.png" />
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Vendor CSS -->
+    <link href="css/vendors.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Theme Custom CSS -->
+    <link rel="stylesheet" href="css/module/define.css">
+    <link rel="stylesheet" href="css/module/typography.css">
+    <link rel="stylesheet" href="css/module/featured-module-test-1.css">
+
+    <!-- Web Fonts  -->
+    <script>
+        WebFontConfig = {
+            google: {
+                families: ['Rubik:300,400,700']
+            },
+            active: function() {
+                $(window).trigger('webfontLoaded');
+            }
+        };
+
+        (function(d) {
+            var wf = d.createElement('script'),
+                s = d.scripts[0];
+            wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+            wf.async = true;
+            s.parentNode.insertBefore(wf, s);
+        })(document);
+    </script>
+</head>
+
+<body class="home-test-1">
+
+    <!-- .site-wrapper -->
+    <div class="site-wrapper">
+        <div class="site-content">
+            <?php include 'module/mnmd-module-test-1.php'; ?>
+        </div>
+    </div><!-- .site-wrapper -->
+
+    <!-- Vendor -->
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/vendors.js"></script>
+
+    <!-- Theme Scripts -->
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+    <!-- Theme Custom Scripts -->
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
