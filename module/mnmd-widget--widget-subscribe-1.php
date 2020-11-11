32bca2f6e44d9d27e10914a0cfe81f3560a98cf2
Tran Tung <trantung1197@gmail.com>
2020-11-11 16:21:08 +0700

    single-1

--- module/mnmd-widget--widget-subscribe-1.php
+++ module/mnmd-widget--widget-subscribe-1.php
@@ -4,16 +4,19 @@
             <div class="subscribe-form subscribe-form--center">
                 <form id="mc4wp-form-2" class="mc4wp-form mc4wp-form-1927" method="post" data-id="1927" data-name="Sidebar form">
                     <div class="mc4wp-form-fields">
-                        <h3>Join our newsletter!</h3>
-                        <p>Enter your email to receive our newsletter.</p>
-                        <p>
-                            <input type="email" name="EMAIL" placeholder="Your email address" required="">
-                        </p>
-                        <p>
-                            <input type="submit" value="Subscribe" class="btn btn-primary">
-                        </p>
-                        <small>Don't worry, we don't spam</small>
-                    </div><label style="display: none !important;">Leave this field empty if you're human: <input type="text" name="_mc4wp_honeypot" value="" tabindex="-1" autocomplete="off"></label><input type="hidden" name="_mc4wp_timestamp" value="1605002185"><input type="hidden" name="_mc4wp_form_id" value="1927"><input type="hidden" name="_mc4wp_form_element_id" value="mc4wp-form-2">
+                        <h3 class="p-x-30">subscribe</h3>
+                        <p class="p-x-30">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                        <div class="form-group form-group__inline">
+                            <input class="input-background--dark flaceholder--left" type="email" name="EMAIL" placeholder="Enter your email" required="">
+                            <button type="submit" class="btn-submit--email"><i class="mdicon mdicon-mail_outline"></i></button>
+                        </div>
+                    </div>
+                    <label style="display: none !important;">Leave this field empty if you're human:
+                        <input type="text" name="_mc4wp_honeypot" value="" tabindex="-1" autocomplete="off">
+                    </label>
+                    <input type="hidden" name="_mc4wp_timestamp" value="1605002185">
+                    <input type="hidden" name="_mc4wp_form_id" value="1927">
+                    <input type="hidden" name="_mc4wp_form_element_id" value="mc4wp-form-2">
                     <div class="mc4wp-response"></div>
                 </form><!-- / MailChimp for WordPress Plugin -->
             </div><!-- .subscribe-form -->
--- single-1.php
+++ single-1.php
@@ -31,6 +31,9 @@ fontweight >= 34: 1.3rem -->
     <!-- Theme CSS -->
     <link href="css/style.css" rel="stylesheet">
 
+    <!-- Icons CSS -->
+    <link href="css/icons.css" rel="stylesheet">
+
     <!-- Theme Custom CSS -->
     <link rel="stylesheet" href="css/module/typography.css">
     <link rel="stylesheet" href="css/module/define.css">
@@ -75,7 +78,7 @@ fontweight >= 34: 1.3rem -->
                 <div class="container">
                     <div class="row">
                         <div class="mnmd-main-col">
-                            <article class="mnmd-block post post--single" itemtype="http://schema.org/Article" itemprop="">
+                            <article class="mnmd-block m-b-0 post post--single" itemtype="http://schema.org/Article" itemprop="">
                                 <div class="page-schema-meta">
                                     <link itemprop="mainEntityOfPage" href="#single-url" />
                                     <meta itemprop="headline" content="Nintendo is reportedly bringing Zelda to your phone this year">
@@ -177,41 +180,187 @@ fontweight >= 34: 1.3rem -->
                                             </div>
                                         </div>
                                     </footer>
-
-                                    <div class="posts-navigation flex-box list-seperated flex-box-2i clearfix">
-                                        <div class="posts-navigation__prev">
-                                            <article class="post post--no-thumb">
-                                                <div class="post__text">
-                                                    <a href="#" class="post__cat post__cat-navigation">Previous</a>
-                                                    <h3 class="post__title f-16 f-w-700">
-                                                        <a href="#">
-                                                            What You Do Today Can Improve
-                                                            All Your Tomorrows
-                                                        </a>
-                                                    </h3>
-                                                </div>
-                                                <a href="#single-url" class="link-overlay"></a>
-                                            </article>
-                                        </div>
-
-                                        <div class="posts-navigation__next">
-                                            <article class="post post--no-thumb">
-                                                <div class="post__text">
-                                                    <a href="#" class="post__cat post__cat-navigation">Next</a>
-                                                    <h3 class="post__title f-16 f-w-700">
-                                                        <a href="#">
-                                                            It Always Seems Impossible
-                                                            Until It’s Done
-                                                        </a>
-                                                    </h3>
-                                                </div>
-                                                <a href="#single-url" class="link-overlay"></a>
-                                            </article>
-                                        </div>
-                                    </div>
                                 </div>
                             </article>
                             <!-- Schema meta -->
+                            <div class="posts-navigation single-entry-section flex-box list-seperated flex-box-2i clearfix">
+                                <div class="posts-navigation__prev">
+                                    <article class="post post--no-thumb">
+                                        <div class="post__text">
+                                            <a href="#" class="post__cat post__cat-navigation">Previous</a>
+                                            <h3 class="post__title f-16 f-w-700">
+                                                <a href="#">
+                                                    What You Do Today Can Improve
+                                                    All Your Tomorrows
+                                                </a>
+                                            </h3>
+                                        </div>
+                                        <a href="#single-url" class="link-overlay"></a>
+                                    </article>
+                                </div>
+
+                                <div class="posts-navigation__next">
+                                    <article class="post post--no-thumb">
+                                        <div class="post__text">
+                                            <a href="#" class="post__cat post__cat-navigation">Next</a>
+                                            <h3 class="post__title f-16 f-w-700">
+                                                <a href="#">
+                                                    It Always Seems Impossible
+                                                    Until It’s Done
+                                                </a>
+                                            </h3>
+                                        </div>
+                                        <a href="#single-url" class="link-overlay"></a>
+                                    </article>
+                                </div>
+                            </div>
+
+                            <div class="author-box author-box--no-background single-entry-section">
+                                <div class="author-box__image">
+                                    <div class="author-avatar author-avatar--no-shadow"><img alt="bkninja" src="https://via.placeholder.com/180x180" class="avatar avatar-180 photo avatar photo" height="180" width="180"></div>
+                                </div>
+                                <div class="author-box__text">
+                                    <div class="author-box__text-group">
+                                        <div class="author-name meta-font">
+                                            <a href="https://thenextmag.bk-ninja.com/default/author/bkninja/" title="Posts by bkninja" rel="author">Susan Browse</a>
+                                        </div>
+                                        <div class="author-info">
+                                            <div class="flex-box grid-gutter-20">
+                                                <div class="author-socials">
+                                                    <ul class="list-unstyled list-horizontal list-space-sm">
+                                                        <li><a href="https://themeforest.net/user/bkninja/portfolio" target="_blank"><i class="mdicon mdicon-twitter"></i><span class="sr-only">Twitter</span></a></li>
+                                                        <li><a href="https://themeforest.net/user/bkninja/portfolio" rel="publisher" target="_blank"><i class="mdicon mdicon-google-plus"></i><span class="sr-only">Google+</span></a></li>
+                                                        <li><a href="https://themeforest.net/user/bkninja/portfolio" target="_blank"><i class="mdicon mdicon-facebook"></i><span class="sr-only">Facebook</span></a></li>
+                                                        <li><a href="http://www.youtube.com/user/https://themeforest.net/user/bkninja/portfolio" target="_blank"><span class="sr-only">Youtube</span></a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="author-bio">BKNinja is a writer based in New York. When he's not writing about apps, marketing, or tech, you can probably catch him eating ice cream.</div>
+                                </div>
+                            </div>
+
+                            <div class="comments-section single-entry-section">
+                                <div id="comments" class="comments-area">
+                                    <h2 class="comments-title comments-title__style-1">03 Comments</h2>
+
+                                    <ol class="comment-list comment-list--style-1">
+                                        <li id="comment-4" class="comment even thread-even depth-1 parent">
+                                            <div id="div-comment-4" class="comment-body">
+                                                <footer class="comment-meta comment-meta--heading-inline">
+                                                    <div class="comment-author vcard comment-meta--left">
+                                                        <img src="http://via.placeholder.com/180x180" class="avatar photo" alt="avatar">
+                                                        <b class="fn">Mabel Agnes</b><span class="says">says:</span>
+                                                        <span class="comment-author__job">Designer</span>
+                                                    </div><!-- .comment-author -->
+                                                    <div class="comment-meta--right">
+                                                        <div class="reply">
+                                                            <a rel="nofollow" class="comment-reply-link" href="#" aria-label="Reply to Sculpture Fan">Reply</a>
+                                                        </div>
+                                                    </div>
+                                                </footer><!-- .comment-meta -->
+                                                <div class="comment-content">
+                                                    <p>Stumptown tote bag Banksy, elit small batch freegan sed. Craft beer elit seitan exercitation, photo booth et 8-bit kale chips proident,</p>
+                                                </div><!-- .comment-content -->
+
+                                                <div class="comment-metadata">
+                                                    <a href="#">
+                                                        <time datetime="2016-10-21T13:31:45+00:00">October 21, 2016 at 1:31 pm</time>
+                                                    </a>
+                                                </div><!-- .comment-metadata -->
+                                            </div><!-- .comment-body -->
+
+                                            <ol class="children">
+                                                <li id="comment-5" class="comment byuser comment-author-melchoyce bypostauthor odd alt depth-2 parent">
+                                                    <div id="div-comment-5" class="comment-body">
+                                                        <footer class="comment-meta comment-meta--heading-inline">
+                                                            <div class="comment-author vcard comment-meta--left">
+                                                                <img src="http://via.placeholder.com/180x180" class="avatar photo" alt="avatar"><a href="#" rel="external nofollow" class="url">Evelyn Fannie</a><span class="says">says:</span>
+                                                                <span class="comment-author__job">Photography</span>
+                                                            </div><!-- .comment-author -->
+                                                            <div class="comment-meta--right">
+                                                                <span class="edit-link">
+                                                                    <a class="comment-edit-link" href="#"><i class="mdicon mdicon-mode_edit"></i>Edit</a>
+                                                                </span>
+                                                                <div class="reply">
+                                                                    <a rel="nofollow" class="comment-reply-link" href="#" aria-label="Reply to Sculpture Fan">Reply</a>
+                                                                </div>
+                                                            </div>
+                                                        </footer><!-- .comment-meta -->
+                                                        <div class="comment-content">
+                                                            <p>Stumptown tote bag Banksy, elit small batch freegan sed. Craft beer elit seitan exercitation, photo booth.</p>
+                                                        </div><!-- .comment-content -->
+
+                                                        <div class="comment-metadata">
+                                                            <a href="#">
+                                                                <time datetime="2016-10-21T13:31:45+00:00">October 21, 2016 at 1:31 pm</time>
+                                                            </a>
+                                                        </div><!-- .comment-metadata -->
+                                                    </div><!-- .comment-body -->
+                                                </li><!-- #comment-## -->
+                                            </ol><!-- .children -->
+                                        </li><!-- #comment-## -->
+                                        <li id="comment-7" class="comment byuser bypostauthor odd alt thread-odd thread-alt depth-1">
+                                            <div id="div-comment-7" class="comment-body">
+                                                <footer class="comment-meta comment-meta--heading-inline">
+                                                    <div class="comment-author comment-meta--left vcard">
+                                                        <img src="http://via.placeholder.com/180x180" class="avatar photo" alt="avatar"><b class="fn">Justin Timberlake</b><span class="says">says:</span>
+                                                        <span class="comment-author__job">Graphics</span>
+                                                    </div><!-- .comment-author -->
+                                                    <div class="comment-meta--right">
+                                                        <span class="edit-link">
+                                                            <a class="comment-edit-link" href="#"><i class="mdicon mdicon-mode_edit"></i>Edit</a>
+                                                        </span>
+                                                        <div class="reply">
+                                                            <a rel="nofollow" class="comment-reply-link" href="#" aria-label="Reply to Sculpture Fan">Reply</a>
+                                                        </div>
+                                                    </div>
+                                                </footer><!-- .comment-meta -->
+                                                <div class="comment-content">
+                                                    <p> Stumptown tote bag Banksy, elit small batch freegan sed. Craft beer elit seitan exercitation, photo booth.</p>
+                                                </div><!-- .comment-content -->
+
+                                                <div class="comment-metadata">
+                                                    <a href="#">
+                                                        <time datetime="2016-10-21T13:31:45+00:00">October 21, 2016 at 1:31 pm</time>
+                                                    </a>
+                                                </div><!-- .comment-metadata -->
+                                            </div><!-- .comment-body -->
+                                        </li><!-- #comment-## -->
+                                    </ol>
+
+                                    <div id="respond" class="comment-respond">
+                                        <h3 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow" id="cancel-comment-reply-link" href="#" style="display: none;">Cancel reply</a></small></h3>
+                                        <form action="#" method="post" id="commentform" class="comment-form" novalidate="">
+                                            <p class="comment-notes"><span id="email-notes">Your email address will not be published.</span> Required fields are marked <span class="required">*</span></p>
+                                            <p class="comment-form-comment">
+                                                <label for="comment">Comment</label>
+                                                <textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525" required="required"></textarea>
+                                            </p>
+                                            <div class="comment-form--group">
+                                                <p class="comment-form-author">
+                                                    <label for="author">Name <span class="required">*</span></label>
+                                                    <input id="author" name="author" type="text" value="" size="30" maxlength="245" required="required">
+                                                </p>
+                                                <p class="comment-form-email">
+                                                    <label for="email">Email <span class="required">*</span></label>
+                                                    <input id="email" name="email" type="email" value="" size="30" maxlength="100" aria-describedby="email-notes" required="required">
+                                                </p>
+                                            </div>
+                                            <p class="comment-form-url">
+                                                <label for="url">Website</label>
+                                                <input id="url" name="url" type="url" value="" size="30" maxlength="200">
+                                            </p>
+                                            <p class="form-submit">
+                                                <input name="submit" type="submit" id="submit" class="submit" value="Submit">
+                                                <input type="hidden" name="comment_post_ID" value="44" id="comment_post_ID">
+                                                <input type="hidden" name="comment_parent" id="comment_parent" value="0">
+                                            </p>
+                                        </form>
+                                    </div><!-- #respond -->
+                                </div><!-- #comments -->
+                            </div>
                         </div>
                         <div class="mnmd-sub-col js-sticky-sidebar" role="complementary">
                             <?php include './module/mnmd-widget--list-post-1.php'; ?>
