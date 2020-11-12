569fd41793145c80858c034584e2eb4bb42f1432
Tran Tung <trantung1197@gmail.com>
2020-11-12 17:29:12 +0700

    training 2

---
+++ module-home-1/featured-module-1.php
@@ -0,0 +1,118 @@
+<div class="mnmd-block mnmd-block--fullwidth mnmd-featured-module-1">
+    <div class="container">
+        <div class="row flex-box">
+            <div class="mnmd-main-col">
+                <div class="mnmd-block__inner mnmd-block--has-posts-stacked">
+                    <article class="post main-post post--horizontal post--horizontal-reverse post--horizontal-middle post--text-over-image">
+                        <div class="post__thumb">
+                            <a href="#single-url"><img src="http://via.placeholder.com/400x500" alt="image"></a>
+                        </div>
+                        <div class="post__text post__text--spacing-1">
+                            <a href="#" class="post__cat post__cat--bg post__cat--bg-yellow">Fashion</a>
+                            <h3 class="post__title f-35 f-w-700">
+                                <a href="#">
+                                    Don’t Talk Unless You Can Improve The
+                                    Silence.
+                                </a>
+                            </h3>
+                            <div class="post__excerpt f-15 ">
+                                Ut sem nulla pharetra diam sit amet. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Consectetur adipiscing elit duis
+                            </div>
+                        </div>
+                    </article>
+
+                    <article class="post sub-post post--overlay post--overlay-bottom">
+                        <div class="background-img background-img--darkened" style="background-image: url(http://via.placeholder.com/250x300);"></div>
+                        <div class="post__text inverse-text">
+                            <div class="post__text-wrap">
+                                <div class="post__text-inner">
+                                    <h3 class="post__title f-16">
+                                        <a href="#">
+                                            Stay Patient And Trust Your Journey
+                                        </a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+
+            <div class="mnmd-sub-col">
+                <div class="mnmd-block__inner background-pattern--gray">
+                    <div class="main-section">
+                        <div class="heading-inside">
+                            <h3 class="heading__title-inside">Hot Pick</h3>
+                        </div>
+                        <article class="post post--vertical post--no-thumb">
+                            <div class="post__text post__text--spacing-1">
+                                <h3 class="post__title f-16 f-w-700">
+                                    <a href="#">
+                                        You Will Eventually Believe
+                                        What You Hear
+                                    </a>
+                                </h3>
+
+                                <div class="post__excerpt f-13">
+                                    Lorem ipsum dolor sit ametert
+                                    consectetur adipiscing elit
+                                    sed do eiusmod.
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="sub-section">
+                        <ol class="posts-list flex-box flex-box-1i flex-space-30 list-style--counter-1">
+                            <li>
+                                <article class="post post--no-thumb post--vertical">
+                                    <div class="post__text post__text--spacing-2">
+                                        <h3 class="post__title f-13 f-w-500">
+                                            <a href="#">
+                                                You Have As Much Laughter As You
+                                                Have Faith in Your Life
+                                            </a>
+                                        </h3>
+                                        <div class="post__meta post__meta--11 post__meta--has-line-before">
+                                            <time class="time" datetime="none" title="none">Feb 19. 2019</time>
+                                        </div>
+                                    </div>
+                                </article>
+                            </li>
+                            <li>
+                                <article class="post post--no-thumb post--vertical">
+                                    <div class="post__text post__text--spacing-2">
+                                        <h3 class="post__title f-13 f-w-500">
+                                            <a href="#">
+                                                You Have As Much Laughter As You
+                                                Have Faith in Your Life
+                                            </a>
+                                        </h3>
+                                        <div class="post__meta post__meta--11 post__meta--has-line-before">
+                                            <time class="time" datetime="none" title="none">Feb 19. 2019</time>
+                                        </div>
+                                    </div>
+                                </article>
+                            </li>
+                            <li>
+                                <article class="post post--no-thumb post--vertical">
+                                    <div class="post__text post__text--spacing-2">
+                                        <h3 class="post__title f-13 f-w-500">
+                                            <a href="#">
+                                                You Have As Much Laughter As You
+                                                Have Faith in Your Life
+                                            </a>
+                                        </h3>
+                                        <div class="post__meta post__meta--11 post__meta--has-line-before">
+                                            <time class="time" datetime="none" title="none">Feb 19. 2019</time>
+                                        </div>
+                                    </div>
+                                </article>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ module-home-1/featured-module-2.php
@@ -0,0 +1,83 @@
+<div class="mnmd-block mnmd-block--fullwidth mnmd-featured-module-2">
+    <div class="container">
+        <div class="row">
+            <div class="mnmd-main-col">
+                <div class="block-heading">
+                    <h4 class="block-heading__title block-heading__title--opacity">Featured</h4>
+                </div>
+                <div class="mnmd-block__inner">
+                    <div class="main-post">
+                        <article class="post post--horizontal post--horizontal-reverse post--horizontal-follow-parent">
+                            <div class="post__thumb">
+                                <a href="#">
+                                    <img src="http://via.placeholder.com/470x690" alt="image">
+                                </a>
+                            </div>
+                            <div class="post__text post__text--spacing-1">
+                                <a href="#" class="post__cat post__cat--bg post__cat--bg-yellow">Fashion</a>
+                                <h3 class="post__title f-30 f-w-700">
+                                    <a href="#">
+                                        A Loving Heart Is The Truest Wisdom.
+                                    </a>
+                                </h3>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div class="sub-posts posts-list flex-box-1i flex-box flex-space-35">
+                        <div class="list-item">
+                            <article class="post post--horizontal post--horizontal-xs">
+                                <div class="post__thumb"><a href="#single-url"><img src="http://via.placeholder.com/100x90" alt="image"></a></div>
+                                <div class="post__text post__text--spacing-2 p-t-5">
+                                    <h3 class="post__title f-14 f-w-500">
+                                        <a href="#">
+                                            Begin, Be Bold And Venture
+                                            To Become Wiser
+                                        </a>
+                                    </h3>
+                                    <div class="post__meta post__meta--11 post__meta--has-line-before line-before--yellow">
+                                        <time class="time" datetime="none" title="none">Feb 19. 2019</time>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="list-item">
+                            <article class="post post--horizontal post--horizontal-xs">
+                                <div class="post__thumb"><a href="#single-url"><img src="http://via.placeholder.com/100x90" alt="image"></a></div>
+                                <div class="post__text post__text--spacing-2 p-t-5">
+                                    <h3 class="post__title f-14 f-w-500">
+                                        <a href="#">
+                                            Begin, Be Bold And Venture
+                                            To Become Wiser
+                                        </a>
+                                    </h3>
+                                    <div class="post__meta post__meta--11 post__meta--has-line-before line-before--yellow">
+                                        <time class="time" datetime="none" title="none">Feb 19. 2019</time>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="list-item">
+                            <article class="post post--horizontal post--horizontal-xs">
+                                <div class="post__thumb"><a href="#single-url"><img src="http://via.placeholder.com/100x90" alt="image"></a></div>
+                                <div class="post__text post__text--spacing-2 p-t-5">
+                                    <h3 class="post__title f-14 f-w-500">
+                                        <a href="#">
+                                            Begin, Be Bold And Venture
+                                            To Become Wiser
+                                        </a>
+                                    </h3>
+                                    <div class="post__meta post__meta--11 post__meta--has-line-before line-before--yellow">
+                                        <time class="time" datetime="none" title="none">Feb 19. 2019</time>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mnmd-sub-col"></div>
+        </div>
+    </div>
+</div>
